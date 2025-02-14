c078e44b80188e8645cf0cf87c721dd28ebf3d12
sebastianto <sebastt.93@gmail.com>
2025-02-13 22:17:03 -0500

    contact blue

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -2,12 +2,12 @@ import { FaInstagram } from "react-icons/fa6";
 
 const Contacto = () => {
   return (
-    <footer className="bg-red-800 text-white py-10 px-6">
+    <footer className="bg-blueBg text-white py-10 px-6">
       <h2 className="text-center text-[clamp(2.625rem,4vw,6.875rem)] font-bold mb-11">
         Contacto
       </h2>
       <div className="flex justify-center">
-        <a href="/" target="_blank">
+        <a href="https://www.instagram.com/thomasguerrero68/" target="_blank">
           <FaInstagram fill="#fff" size={44} />
         </a>
       </div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,7 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        blueBg: "#50e0e6",
       },
     },
   },
